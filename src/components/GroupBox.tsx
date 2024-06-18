a8724318adec20e7503d16958f1f25c5444a3c1e
Karol Słupiński <karol.slupinski.97@gmail.com>
2024-06-18 23:09:07 +0200

    feat: create GroupBox

---
+++ src/components/GroupBox.tsx
@@ -0,0 +1,34 @@
+import { RiRefund2Line } from "react-icons/ri";
+import { GroupState } from "../store/groupSlice";
+
+type GroupBoxProps = {
+  group: GroupState;
+};
+
+export const GroupBox = ({ group }: GroupBoxProps) => {
+  const sum = Number(
+    group.members
+      .reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.amount;
+      }, 0)
+      .toFixed(2)
+  );
+
+  const isUserOwed = sum > 0;
+
+  return (
+    <div className="flex flex-col h-52 w-64 p-2 px-3 content-between border border-slate-300 rounded-lg bg-slate-50 shadow-xl hover:border-2 hover:border-blue-500 cursor-pointer">
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-lg font-semibold">{group.name}</h1>
+        <RiRefund2Line size={24} />
+      </div>
+      <div className="grid grid-rows-1 bt h-full items-end">
+        <p>{isUserOwed ? "You are owed:" : "You owe:"}</p>
+        <p className={`text-6xl ${isUserOwed ? "text-green-500" : "text-red-500"} font-semibold`}>
+          {sum}
+        </p>
+        <p>Number of members: {group.members.length}</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,9 +1,16 @@
+import { GroupBox } from "../components/GroupBox";
 import { SidebarMenu } from "../components/SidebarMenu";
+import { initialGroupState } from "../store/initialGroupState";
 
 export const Overview = () => {
   return (
-    <div className="h-screen">
+    <div className="flex flex-row h-screen ">
       <SidebarMenu />
+      <div className="grid grid-cols-3 gap-10 p-16">
+        {initialGroupState.map((singleInitialGroupState) => {
+          return <GroupBox group={singleInitialGroupState} />;
+        })}
+      </div>
     </div>
   );
 };
